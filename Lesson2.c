593ca56c20b4a85c4be562d40ccf49d70a29e390
Alexandr Grebenkov <alexandrgrebenkov@gmail.com>
2019-07-06 14:22:49 +0500

    Выполнены 1 и 2 задания

---
+++ Lesson2.c
@@ -0,0 +1,41 @@
+#include "Lesson2.h"
+
+char str[20];
+
+int L2_exec_1(int input)
+{
+	if (input < 2)
+		return input;
+	int val = L2_exec_1(input / 2) * 10 + input % 2;
+	return val;
+}
+
+int L2_exec_2a(int a, int b)
+{
+	int result = 1;
+	while (b-- > 0)
+	{
+		result *= a;
+	}
+	return result;
+}
+
+int L2_exec_2b(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	return L2_exec_2b(a, --b) * a;
+}
+
+int L2_exec_2c(int a, int b)
+{
+	if (b == 0)
+		return 1;
+	if (b % 2 == 1)
+		return L2_exec_2c(a, b - 1) * a;
+	else
+	{
+		int tmp = L2_exec_2c(a, b / 2);
+		return tmp * tmp;
+	}
+}
